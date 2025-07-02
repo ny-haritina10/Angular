14e1efb5e00bc601ba1fefe41ab76424fec2d770
Ny Haritina <142341722+ny-haritina10@users.noreply.github.com>
2025-07-02 09:35:30 +0300

    [clean] reset Angular Project

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 export const routes: Routes = [
-  { path: '', component: EmployeeListComponent }
 ];
